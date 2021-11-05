825421054aebe1c93d827e76ca1eca9d6f811800
spinto1 <70720130+spinto1@users.noreply.github.com>
2021-11-05 14:00:32 -0500

    Update detectColors.py
    Added code to detect specifically red, blue, black, white

--- detectColors.py
+++ detectColors.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 import numpy as np
 
@@ -18,26 +19,32 @@ while True:
     lower_blue = np.array([90, 50, 50])
     upper_blue = np.array([130, 255, 255])
 
+    # set color range of white
+    lower_white = np.array([0, 0, 0])
+    upper_white = np.array([0, 0, 255])
+
     # set color range of red
     lower_red = np.array([136, 87, 111])
     upper_red = np.array([180, 255, 255])
 
-    # set color range of green
-    lower_green = np.array([25, 52, 72])
-    upper_green = np.array([102, 255, 255])
+    # set color range of black
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([255, 255, 0])
 
     # creating masks for each color
     blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    white_mask = cv2.inRange(hsv, lower_white, lower_white)
     red_mask = cv2.inRange(hsv, lower_red, upper_red)
+    black_mask = cv2.inRange(hsv, lower_black, upper_black)
 
     # calculating sizes of masks
     blue_size = cv2.countNonZero(blue_mask)
-    green_size = cv2.countNonZero(green_mask)
+    white_size = cv2.countNonZero(white_mask)
     red_size = cv2.countNonZero(red_mask)
+    black_size = cv2.countNonZero(black_mask)
 
-    # identify which mask is largest
-    sizes = [blue_size, green_size, red_size]
+    # identify which color is most dominant and store value
+    sizes = [blue_size, white_size, red_size, black_size]
     max_value = max(sizes)
 
     # print out dominant color
@@ -45,13 +52,17 @@ while True:
         result = cv2.bitwise_and(frame, frame, mask=blue_mask)
         print("blue is dominant")
 
-    if max_value == green_size:
-        result = cv2.bitwise_and(frame, frame, mask=green_mask)
-        print("green is dominant")
+    if max_value == white_size:
+        result = cv2.bitwise_and(frame, frame, mask=white_mask)
+        print("white is dominant")
 
     if max_value == red_size:
         result = cv2.bitwise_and(frame, frame, mask=red_mask)
         print("red is dominant")
+
+    else:
+        result = cv2.bitwise_and(frame, frame, mask=black_mask)
+        print("black is dominant")
 
     cv2.imshow('Frame', result)
 
