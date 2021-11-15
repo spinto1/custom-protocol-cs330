a4a8ad4539e13ee9726f288f885669e8f76ff619
spinto1 <70720130+spinto1@users.noreply.github.com>
2021-11-14 20:24:24 -0600

    Split webcam into 4 quadrants. Detects dominant color in each quadrant

--- detectColors.py
+++ detectColors.py
@@ -25,7 +25,7 @@ def create_mask(src, lower, upper):
 
 
 # determine dominant color in frame
-def detect_dominant_color(b_mask, g_mask, r_mask, y_mask):
+def detect_dominant_color(section, b_mask, g_mask, r_mask, y_mask):
     # calculating size of masks
     b_size = cv2.countNonZero(b_mask)
     g_size = cv2.countNonZero(g_mask)
@@ -35,32 +35,56 @@ def detect_dominant_color(b_mask, g_mask, r_mask, y_mask):
     mask_sizes = [b_size, g_size, r_size, y_size]
     max_color = max(mask_sizes)
 
-    if b_size > 0:
-        print("Blue detected")
+    if max_color == b_size:
+        print("Blue is dominant in")
+        return
 
-    if g_size > 0:
-        print("Green detected")
+    if max_color == g_size:
+        print("Green is dominant in")
+        return
 
-    if r_size > 0:
-        print("Red detected")
+    if max_color == r_size:
+        print("Red is dominant in")
+        return
 
-    if y_size > 0:
-        print("Yellow detected")
+    if max_color == y_size:
+        print("Yellow is dominant")
+        return
 
-    b_output = cv2.bitwise_and(frame, frame, mask=b_mask)
-    g_output = cv2.bitwise_and(frame, frame, mask=g_mask)
-    r_output = cv2.bitwise_and(frame, frame, mask=r_mask)
-    y_output = cv2.bitwise_and(frame, frame, mask=y_mask)
 
-    return b_output, g_output, r_output, y_output
+# Create subsections areas of camera
+# Source: https://stackoverflow.com/questions/23720875/how-to-draw-a-rectangle-around-a-region-of-interest-in-python
+upper_left_1 = (0, 0)
+upper_left_2 = (320, 240)
 
+upper_right_1 = (320, 0)
+upper_right_2 = (640, 240)
+
+bottom_left_1 = (0, 240)
+bottom_left_2 = (320, 480)
+
+bottom_right_1 = (320, 240)
+bottom_right_2 = (640, 480)
+
+binary_data = ''
 
 while True:
     # read frame of webcam
     ret, frame = webcam.read()
+    # Get frame size:
+    # print('Resolution: ' + str(frame.shape[0]) + ' x ' + str(frame.shape[1]))
+
+    # Create webcam subsections
+    upper_left_frame = frame[upper_left_1[1]: upper_left_2[1], upper_left_1[0]: upper_left_2[0]]
+    upper_right_frame = frame[upper_right_1[1]: upper_right_2[1], upper_right_1[0]: upper_right_2[0]]
+    bottom_left_frame = frame[bottom_left_1[1]: bottom_left_2[1], bottom_left_1[0]: bottom_left_2[0]]
+    bottom_right_frame = frame[bottom_right_1[1]: bottom_right_2[1], bottom_right_1[0]: bottom_right_2[0]]
 
     # convert frame to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    ul = cv2.cvtColor(upper_left_frame, cv2.COLOR_BGR2HSV)
+    ur = cv2.cvtColor(upper_right_frame, cv2.COLOR_BGR2HSV)
+    bl = cv2.cvtColor(bottom_left_frame, cv2.COLOR_BGR2HSV)
+    br = cv2.cvtColor(bottom_right_frame, cv2.COLOR_BGR2HSV)
 
     # set color ranges
     lower_blue, upper_blue = set_color_range([0, 130, 200])
@@ -68,23 +92,26 @@ while True:
     lower_red, upper_red = set_color_range([230, 25, 75])
     lower_yellow, upper_yellow = set_color_range([255, 225, 25])
 
-    # creating color masks
-    blue_mask = create_mask(hsv, lower_blue, upper_blue)
-    green_mask = create_mask(hsv, lower_green, upper_green)
-    red_mask = create_mask(hsv, lower_red, upper_red)
-    yellow_mask = create_mask(hsv, lower_yellow, upper_yellow)
+    subsections = [ul, ur, bl, br]  # list of subsections
 
-    b_result, g_result, r_result, y_result = detect_dominant_color(blue_mask, green_mask, red_mask, yellow_mask)
+    # creates mask and finds dominant color for each subsection
+    for frame in subsections:
+        # creating color masks
+        blue_mask = create_mask(frame, lower_blue, upper_blue)
+        green_mask = create_mask(frame, lower_green, upper_green)
+        red_mask = create_mask(frame, lower_red, upper_red)
+        yellow_mask = create_mask(frame, lower_yellow, upper_yellow)
 
-    # displaying color detection for testing purposes
-    cv2.imshow('Red', b_result)
-    cv2.imshow('Green', g_result)
-    cv2.imshow('Red', r_result)
-    cv2.imshow('Yellow', yellow_mask)
+        detect_dominant_color(frame, blue_mask, green_mask, red_mask, yellow_mask)
+
+    cv2.imshow('Upper Left', upper_left_frame)
+    cv2.imshow('Upper Right', upper_right_frame)
+    cv2.imshow('Bottom Left', bottom_left_frame)
+    cv2.imshow('Bottom Right', bottom_right_frame)
 
     # wait 5 seconds between each iteration for testing purposes
     time.sleep(5)
-    print("-------------")
+    print("-------------------")
 
     # kill program by pressing "q" key
     if cv2.waitKey(1) == ord('q'):
